df105664742cec2c83085c11bcd9bbd1ed264a20
Jihyun Kim <48214314+rabBit64@users.noreply.github.com>
2023-01-09 10:33:09 +0900

    [Bronze II] Title: 막대기, Time: 64 ms, Memory: 34328 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,18 +1,13 @@
 import sys
-N = int(sys.stdin.readline())
-stack = []
-for _ in range(N):
-  num = int(sys.stdin.readline())
-  if len(stack)>0:
-    if stack[-1]<=num:
-      #큰 경우엔 자신보다 클때까지 뺀다
-      while True:
-        if len(stack)==0 or stack[-1]>num: 
-          break
-        stack.pop()
-      stack.append(num)
-    else:
-      stack.append(num)
-  else:
-    stack.append(num)
-print(len(stack))
+def sol():
+  input = sys.stdin.readline
+  N = int(input())
+  arr = [int(input()) for i in range(N)]
+  M = 0
+  ans = 0
+  for num in reversed(arr):
+    if num>M:
+      M=num
+      ans+=1
+  return ans
+print(sol())
