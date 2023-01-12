59d6528c486040622cb1f0f58b8cb091768c3791
Jihyun Kim <48214314+rabBit64@users.noreply.github.com>
2023-01-12 10:43:36 +0900

    [Silver I] Title: 카잉 달력, Time: 1504 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,14 +1,12 @@
+def calendar(M,N,x,y):
+  while x<=M*N:
+    if (x-y)%N==0:
+      return x
+    x+=M
+  return -1
+
 T = int(input())
 for _ in range(T):
   M,N,x,y = map(int,input().split())
+  print(calendar(M,N,x,y))
 
-  valid = False
-  while x<=M*N:
-    if (x-y)%N==0:
-      valid = True
-      break
-    x+=M
-  if valid:
-    print(x)
-  else:
-    print(-1)
