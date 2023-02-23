c10c1f816aa08b152825c3e81041b597aba45a5d
Jihyun Kim <48214314+rabBit64@users.noreply.github.com>
2023-02-23 16:15:48 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,15 @@
+n = int(input())
+'''
+2xn
+dp[1]=1
+dp[2]=3
+dp[3]=5
+dp[4]=11
+dp[i]=dp[i-1]+2*dp[i-2]
+'''
+dp=[0 for _ in range(1001)]
+dp[0]=dp[1]=1
+for i in range(1001):
+  if dp[i]==0:
+    dp[i]=dp[i-1]+2*dp[i-2]
+print(dp[n]%10007)
