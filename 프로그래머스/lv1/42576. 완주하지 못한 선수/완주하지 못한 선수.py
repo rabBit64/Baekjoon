97afa37337f1fe0fb579178773bf753fa592c59e
Jihyun Kim <48214314+rabBit64@users.noreply.github.com>
2023-07-30 22:55:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 131.84 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,9 @@
+import collections
 def solution(participant, completion):
     answer = ''
-    dic = {}
-    for name in participant:
-        if name not in dic:
-            dic[name]=1
-        else:
-            dic[name]+=1
-    #print(dic)
-    for name in completion:
-        dic[name]-=1
-    #print(dic)
-    for key,value in dic.items():
-        if value>0:
-            answer=key
+    participant.sort()
+    completion.sort()
+    result = collections.Counter(participant)-collections.Counter(completion)
+    #print(list(result)[0])
+    answer = list(result)[0]
     return answer
