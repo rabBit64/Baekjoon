2447bc70ecabbb8295250182eb98c46671116c4e
Jihyun Kim <48214314+rabBit64@users.noreply.github.com>
2024-05-18 22:06:39 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,13 +1,17 @@
 def solution(progresses, speeds):
     answer = []
-    while progresses:
-        cnt=0
-        while progresses and progresses[0]>=100:
-            cnt+=1
+    days=0
+    cnt=0
+    while len(progresses)>0:
+        if(progresses[0]+days*speeds[0])>=100:
             progresses.pop(0)
             speeds.pop(0)
-        progresses = [progresses[i]+speeds[i] for i in range(len(progresses))]
-        print(progresses)
-        if cnt:
-            answer.append(cnt)
+            cnt+=1
+        else:
+            if cnt>0:
+                answer.append(cnt)
+                cnt=0
+            else:
+                days+=1
+    answer.append(cnt)
     return answer
